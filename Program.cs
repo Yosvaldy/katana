60320940ef6c69d45b66a83849bccfaca3eeeea2
Yosvaldy Fernandez <yosvaldy2011@gmail.com>
2017-11-13 15:18:42 -0500

    Adding middlewares

--- Program.cs
+++ Program.cs
@@ -27,6 +27,22 @@ namespace Katana
     {
         public void Configuration(IAppBuilder app)
         {
+            //app.Use(async (enviroment, next) =>
+            //{
+            //    foreach (var pair in enviroment.Environment)
+            //    {
+            //        Console.WriteLine("{0}:{1}", pair.Key, pair.Value);
+            //    }
+            //    await next();
+            //});
+
+            app.Use(async (enviroment, next) =>
+            {
+                Console.WriteLine("Requesting: " + enviroment.Request.Path);
+                await next();
+                Console.WriteLine("Response: " + enviroment.Response.StatusCode);
+            });
+
             app.UseHelloWorld();
         }
     }
